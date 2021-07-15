8fb400d0163c469bc1769f6c3500065dfb5b7c9b
Aaron Foley <afoley@linq.com>
2021-07-15 16:30:36 -0400

    Got basics of Project explorer working
    Got basics of Project explorer working

--- Pictomancer/Pictomancer/ViewModels/MainViewModel.cs
+++ Pictomancer/Pictomancer/ViewModels/MainViewModel.cs
@@ -1,5 +1,9 @@
-﻿using Pictomancer.Mvvm;
+﻿using System;
+using System.Windows;
+using Microsoft.Win32;
+using Pictomancer.Mvvm;
 using Pictomancer.Views;
+using Relm.Maps;
 using Relm.Pipeline.Models;
 
 namespace Pictomancer.ViewModels
@@ -7,11 +11,27 @@ namespace Pictomancer.ViewModels
     public class MainViewModel
         : ViewModel
     {
-        public PictomancerProject Project { get; set; }
+        public bool IsDirty { get; set; }
+        
+        #region Dependency Properties
+
+        public static readonly DependencyProperty ProjectProperty = DependencyProperty.Register("Project", typeof(ProjectViewModel), typeof(MainViewModel), new PropertyMetadata(default(ProjectViewModel)));
+
+        public ProjectViewModel Project
+        {
+            get => (ProjectViewModel) GetValue(ProjectProperty);
+            set => SetValue(ProjectProperty, value);
+        }
+
+        #endregion
 
         #region Command Properties
 
         public Command NewProjectCommand { get; set; }
+        public Command OpenProjectCommand { get; set; }
+        public Command SaveProjectCommand { get; set; }
+        public Command NewMapCommand { get; set; }
+        public Command DeleteMapCommand { get; set; }
 
         #endregion
 
@@ -19,13 +39,17 @@ namespace Pictomancer.ViewModels
 
         public MainViewModel()
         {
-            Project = new PictomancerProject();
+            Project = new ProjectViewModel();
             InitializeCommands();
         }
 
         private void InitializeCommands()
         {
             NewProjectCommand = new Command(NewProject);
+            OpenProjectCommand = new Command(OpenProject);
+            SaveProjectCommand = new Command(SaveProject);
+            NewMapCommand = new Command(NewMap);
+            DeleteMapCommand = new Command(DeleteMap);
         }
 
         #endregion
@@ -42,13 +66,87 @@ namespace Pictomancer.ViewModels
             if (!view.ShowDialog().GetValueOrDefault()) return;
 
             var results = view.ViewModel;
-            Project = new PictomancerProject
+            Project = new ProjectViewModel
             {
                 Name = results.ProjectName
             };
-            ((MainWindow) Owner).Title = $"Pictomancer - * [ {Project.Name} ]";
+            Project.CreateNewMap();
+            SetIsDirty(true);
+        }
+
+        public void OpenProject()
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter = "Pictomancer Files (*.pictomancer)|*.pictomancer|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+
+            if (!dialog.ShowDialog((MainWindow)Owner).GetValueOrDefault()) return;
+
+            
+        }
+
+        public void SaveProject()
+        {
+            if (string.IsNullOrEmpty(Project.Path))
+            {
+                SaveNewProject();
+                return;
+            }
+
+            SetIsDirty(false);
+        }
+
+        public void SaveNewProject()
+        {
+            var dialog = new SaveFileDialog
+            {
+                FileName = $"{Project.Name.Replace(" ", "_")}.pictomancer",
+                Filter = "Pictomancer Files (*.pictomancer)|*.pictomancer|All files (*.*)|*.*",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+            };
+
+            if (!dialog.ShowDialog((MainWindow)Owner).GetValueOrDefault()) return;
+
+            SetIsDirty(false);
+        }
+
+        public void NewMap()
+        {
+            if (Project.Name == "Empty Project")
+            {
+                MessageBox.Show("Unable to create new map because there is no project", "Pictomancer");
+                return;
+            }
+
+            Project.CreateNewMap();
+        }
+
+        public void DeleteMap()
+        {
+            if (Project.Name == "Empty Project")
+            {
+                MessageBox.Show("Unable to delete map because there is no project", "Pictomancer");
+                return;
+            }
+
+            if (Project.SelectedItem.GetType() != typeof(Map))
+            {
+                MessageBox.Show("Unable to delete map because no map selected", "Pictomancer");
+                return;
+            }
+
+            Project.DeleteMap((Map)Project.SelectedItem);
         }
 
         #endregion
+
+        public void SetIsDirty(bool value)
+        {
+            IsDirty = value;
+            var flag = IsDirty ? "* " : "";
+            ((MainWindow)Owner).Title = $"Pictomancer - {flag}[ {Project.Name} ]";
+        }
     }
 }
---
+++ Pictomancer/Pictomancer/ViewModels/ProjectViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+using Pictomancer.Mvvm;
+using Relm.Maps;
+
+namespace Pictomancer.ViewModels
+{
+    public class ProjectViewModel
+        : ViewModel
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+        public string Title => $"Project '{Name}'";
+
+        #region Dependency Properties
+
+        public static readonly DependencyProperty MapsProperty = DependencyProperty.Register("Maps", typeof(ObservableCollection<Map>), typeof(ProjectViewModel), new PropertyMetadata(default(ObservableCollection<Map>)));
+        public ObservableCollection<Map> Maps
+        {
+            get => (ObservableCollection<Map>) GetValue(MapsProperty);
+            set => SetValue(MapsProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty = DependencyProperty.Register("SelectedItem", typeof(object), typeof(ProjectViewModel), new PropertyMetadata(default(object)));
+
+        public object SelectedItem
+        {
+            get => GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+
+        #endregion
+
+        public ProjectViewModel()
+        {
+            Name = "Empty Project";
+            Maps = new ObservableCollection<Map>();
+        }
+
+        public Map CreateNewMap()
+        {
+            var index = Maps.Count + 1;
+            var name = $"Map {index}";
+            var map = new Map
+            {
+                Name = name
+            };
+            Maps.Add(map);
+            return map;
+        }
+
+        public void DeleteMap(Map selectedMap)
+        {
+            Maps.Remove(selectedMap);
+        }
+    }
+}
