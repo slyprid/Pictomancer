88607d1534dd33dbfc7ad8b5e7243264e4d4a1a4
Aaron Foley <afoley@linq.com>
2021-07-20 16:18:49 -0400

    Added ability to select tiles from tiles page
    Added ability to select tiles from tiles page

--- Pictomancer/Pictomancer/Graphics/TileCanvas.cs
+++ Pictomancer/Pictomancer/Graphics/TileCanvas.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections.Generic;
+using System.Windows.Input;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
 using MonoGame.Extended.Sprites;
 using MonoGame.Extended.TextureAtlases;
+using MonoGame.Extended.Tweening;
 using MonoGame.Framework.WpfInterop;
 using MonoGame.Framework.WpfInterop.Input;
+using Pictomancer.Models;
 using Pictomancer.ViewModels;
+using Relm.Extensions;
 
 namespace Pictomancer.Graphics
 {
@@ -18,80 +22,105 @@ namespace Pictomancer.Graphics
         private WpfMouse _mouse;
         private SpriteBatch _spriteBatch;
         private TextureAtlas _tileset;
+        private InputModel _input;
+        private Vector2 _mousePosition;
+        private float _mx;
+        private float _my;
+        private readonly ColorEx _cursorColor;
+        private readonly Tweener _tweener;
+        private string primarySelected = "0-0";
+        private string secondarySelected = "1-0";
+
+        public int TileWidth = 32;
+        public int TileHeight = 32;
+
+        public TileCanvas()
+        {
+            _input = new InputModel();
+            _cursorColor = new ColorEx(Color.Yellow);
+            _tweener = new Tweener();
+            _tweener.TweenTo(_cursorColor, x => x.Value3, new Vector3(1f, 0, 0f), 0.25f, 0.025f).RepeatForever(0.2f).AutoReverse().Easing(EasingFunctions.Linear);
+        }
 
         protected override void Initialize()
         {
-            // must be initialized. required by Content loading and rendering (will add itself to the Services)
-            // note that MonoGame requires this to be initialized in the constructor, while WpfInterop requires it to
-            // be called inside Initialize (before base.Initialize())
             _graphicsDeviceManager = new WpfGraphicsDeviceService(this);
 
-            // wpf and keyboard need reference to the host control in order to receive input
-            // this means every WpfGame control will have it's own keyboard & mouse manager which will only react if the mouse is in the control
             _keyboard = new WpfKeyboard(this);
             _mouse = new WpfMouse(this);
 
-            // must be called after the WpfGraphicsDeviceService instance was created
             base.Initialize();
 
-            // content loading now possible
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             var regions = new Dictionary<string, Rectangle>();
 
-            var i = 0;
-            for (var y = 0; y < 32; y++)
+            for (var y = 0; y < TileHeight; y++)
             {
-                for (var x = 0; x < 32; x++)
+                for (var x = 0; x < TileWidth; x++)
                 {
-                    regions.Add($"{i++}", new Rectangle(x * 32, y * 32, 32, 32));
+                    regions.Add($"{x}-{y}", new Rectangle(x * TileWidth, y * TileHeight, TileWidth, TileHeight));
                 }
             }
 
             _tileset = new TextureAtlas("Tileset 1", Content.Load<Texture2D>("Tileset"), regions);
         }
 
-        protected override void Update(GameTime time)
+        protected override void Update(GameTime gameTime)
         {
-            // every update we can now query the keyboard & mouse for our WpfGame
-            var mouseState = _mouse.GetState();
-            var keyboardState = _keyboard.GetState();
+            _input.PreviousKeyboardState = _input.CurrentKeyboardState;
+            _input.PreviousMouseState = _input.CurrentMouseState;
+            _input.CurrentKeyboardState = _keyboard.GetState();
+            _input.CurrentMouseState = _mouse.GetState();
+
+            _mousePosition = new Vector2(_input.CurrentMouseState.X, _input.CurrentMouseState.Y);
+
+            var xp = 144;
+            var yp = 8;
+            var ox = _mousePosition.X < xp ? 0 : _mousePosition.X - xp;
+            var oy = _mousePosition.Y < yp ? 0 : _mousePosition.Y - yp;
+
+            _mx = (int)(ox / TileWidth) * TileWidth;
+            _my = (int)(oy / TileHeight) * TileHeight;
+
+            _tweener.Update(gameTime.GetElapsedSeconds());
+
+            var tx = (int)(_mx / TileWidth);
+            var ty = (int) (_my / TileHeight);
+            if (_input.IsMousePressed(MouseButton.Left))
+            {
+                primarySelected = $"{tx}-{ty}";
+            }
+            else if (_input.IsMousePressed(MouseButton.Right))
+            {
+                secondarySelected = $"{tx}-{ty}";
+            }
         }
 
-        protected override void Draw(GameTime time)
+        protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.White);
 
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(_tileset.GetRegion("0"), new Rectangle(8, 8, 64, 64), Color.White);
-            _spriteBatch.Draw(_tileset.GetRegion("1"), new Rectangle(72, 72, 64, 64), Color.White);
+            _spriteBatch.Draw(_tileset.GetRegion(primarySelected), new Rectangle(8, 8, TileWidth * 2, TileHeight * 2), Color.White);
+            _spriteBatch.Draw(_tileset.GetRegion(secondarySelected), new Rectangle(72, 72, TileWidth * 2, TileHeight * 2), Color.White);
 
-            _spriteBatch.DrawRectangle(8, 8, 64, 64, Color.Black, 1f, 0f);
-            _spriteBatch.DrawRectangle(72, 72, 64, 64, Color.Black, 1f, 0f);
+            _spriteBatch.DrawRectangle(8, 8, TileWidth * 2, TileHeight * 2, Color.Black, 1f, 0f);
+            _spriteBatch.DrawRectangle(72, 72, TileWidth * 2, TileHeight * 2, Color.Black, 1f, 0f);
 
 
             var w = 1632;
             var h = 192;
-            var tw = 32;
-            var th = 32;
+            var tw = TileWidth;
+            var th = TileHeight;
 
             var xp = 144;
             var yp = 8;
             foreach (var texture in _tileset)
             {
-                _spriteBatch.Draw(texture, new Rectangle(xp, yp, tw, th), Color.White);
-                xp += tw;
-                if (xp >= w)
-                {
-                    xp = 144;
-                    yp += th;
-                }
-
-                if (yp >= h)
-                {
-                    break;
-                }
+                if (texture.X + xp > w || texture.Y + yp > h) continue;
+                _spriteBatch.Draw(texture, new Rectangle(texture.X + xp, texture.Y + yp, tw, th), Color.White);
             }
 
             for (var y = 8; y < h; y += th)
@@ -101,6 +130,8 @@ namespace Pictomancer.Graphics
                     _spriteBatch.DrawRectangle(new RectangleF(x, y, tw, th), Color.Black);
                 }
             }
+
+            _spriteBatch.DrawRectangle(new RectangleF(_mx + xp, _my + yp, TileWidth, TileHeight), _cursorColor.Color);
 
             _spriteBatch.End();
         }
--- Pictomancer/Pictomancer/Models/InputModel.cs
+++ Pictomancer/Pictomancer/Models/InputModel.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Input;
+﻿using System.Windows.Input;
+using Microsoft.Xna.Framework.Input;
 
 namespace Pictomancer.Models
 {
@@ -9,6 +10,36 @@ namespace Pictomancer.Models
 
         public KeyboardState CurrentKeyboardState { get; set; }
         public KeyboardState PreviousKeyboardState { get; set; }
+
+        public bool IsMousePressed(MouseButton button)
+        {
+            if (button == MouseButton.Left && PreviousMouseState.LeftButton == ButtonState.Released && CurrentMouseState.LeftButton == ButtonState.Pressed) return true;
+            if (button == MouseButton.Middle && PreviousMouseState.MiddleButton == ButtonState.Released && CurrentMouseState.MiddleButton == ButtonState.Pressed) return true;
+            if (button == MouseButton.Right && PreviousMouseState.RightButton == ButtonState.Released && CurrentMouseState.RightButton == ButtonState.Pressed) return true;
+            if (button == MouseButton.XButton1 && PreviousMouseState.XButton1 == ButtonState.Released && CurrentMouseState.XButton1 == ButtonState.Pressed) return true;
+            if (button == MouseButton.XButton2 && PreviousMouseState.XButton2 == ButtonState.Released && CurrentMouseState.XButton2 == ButtonState.Pressed) return true;
+            return false;
+        }
+
+        public bool IsMouseReleased(MouseButton button)
+        {
+            if (button == MouseButton.Left && PreviousMouseState.LeftButton == ButtonState.Pressed && CurrentMouseState.LeftButton == ButtonState.Released) return true;
+            if (button == MouseButton.Middle && PreviousMouseState.MiddleButton == ButtonState.Pressed && CurrentMouseState.MiddleButton == ButtonState.Released) return true;
+            if (button == MouseButton.Right && PreviousMouseState.RightButton == ButtonState.Pressed && CurrentMouseState.RightButton == ButtonState.Released) return true;
+            if (button == MouseButton.XButton1 && PreviousMouseState.XButton1 == ButtonState.Pressed && CurrentMouseState.XButton1 == ButtonState.Released) return true;
+            if (button == MouseButton.XButton2 && PreviousMouseState.XButton2 == ButtonState.Released && CurrentMouseState.XButton2 == ButtonState.Released) return true;
+            return false;
+        }
+
+        public bool IsMouseHeld(MouseButton button)
+        {
+            if (button == MouseButton.Left && PreviousMouseState.LeftButton == ButtonState.Pressed && CurrentMouseState.LeftButton == ButtonState.Pressed) return true;
+            if (button == MouseButton.Middle && PreviousMouseState.MiddleButton == ButtonState.Pressed && CurrentMouseState.MiddleButton == ButtonState.Pressed) return true;
+            if (button == MouseButton.Right && PreviousMouseState.RightButton == ButtonState.Pressed && CurrentMouseState.RightButton == ButtonState.Pressed) return true;
+            if (button == MouseButton.XButton1 && PreviousMouseState.XButton1 == ButtonState.Pressed && CurrentMouseState.XButton1 == ButtonState.Pressed) return true;
+            if (button == MouseButton.XButton2 && PreviousMouseState.XButton2 == ButtonState.Pressed && CurrentMouseState.XButton2 == ButtonState.Pressed) return true;
+            return false;
+        }
 
     }
 }
